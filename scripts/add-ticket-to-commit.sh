c3eb22f3db3b354ad81515513fe9018e3a018c03
Roelof Roos <roelof@motivo.nl>
2021-05-24 14:48:07 +0200

    Don't fix messages that are in autosquash format

--- scripts/add-ticket-to-commit.sh
+++ scripts/add-ticket-to-commit.sh
@@ -3,6 +3,9 @@
 FILE="$1"
 
 MESSAGE="$(cat $FILE)"
+if [[ "$MESSAGE" = "fixup!*" || "$MESSAGE" == "squash!*" ]]; then
+	exit 0;
+fi
 
 TICKET="$(git rev-parse --abbrev-ref HEAD | grep -Eo '^(\w+/)?(\w+[-_])?[0-9]+' | grep -Eo '(\w+[-])?[0-9]+' | tr "[:lower:]" "[:upper:]")"
 if [[ $TICKET == "" || "$MESSAGE" == "$TICKET -"* || "$MESSAGE" == "$TICKET:"* || "$MESSAGE" == "[$TICKET]"* ]]; then
